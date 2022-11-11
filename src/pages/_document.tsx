c12c99f66d3247446130b7b2b1d6f0d76906b760
matheusthiesen <matheusreis314@gmail.com>
2022-11-11 16:25:32 -0300

    Ajustes PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,27 @@ export default class MyDocument extends Document {
           />
           <link rel="icon" href="/favicon.png" />
           <meta name="theme-color" content="#E5122A" />
+
+          <meta name="application-name" content="Catálogo Alpar" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Catálogo" />
+          <meta
+            name="description"
+            content="Gerar Catálogo dos produtos disponíveis para venda"
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#E5122A" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#E5122A" />
         </Head>
         <body style={{ overflow: "auto" }}>
           <ColorModeScript initialColorMode={theme.initialColorMode} />
