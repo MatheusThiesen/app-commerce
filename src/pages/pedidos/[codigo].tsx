b949238e3972a5b6ea4580514e04aca5999fa694
MatheusThiesen <matheusreis314@gmail.com>
2025-03-29 13:16:31 -0300

    vendedor pendente apenas no pedido diferenciado em aberto

--- src/pages/pedidos/[codigo].tsx
+++ src/pages/pedidos/[codigo].tsx
@@ -621,33 +621,34 @@ export default function Order() {
               </Box>
 
               {order.vendedorPendenteDiferenciadoCodigo !==
-                user?.vendedorCodigo && (
-                <DetailBox className="w-full">
-                  <DetailBoxTitle>Pendente de aprovação</DetailBoxTitle>
+                user?.vendedorCodigo &&
+                order?.situacaoPedido?.codigo === 6 && (
+                  <DetailBox className="w-full">
+                    <DetailBoxTitle>Pendente de aprovação</DetailBoxTitle>
 
-                  <InputBase
-                    name="sellerApproval"
-                    label="Código"
-                    defaultValue={order.vendedorPendenteDiferenciado?.codigo}
-                    readOnly
-                  />
+                    <InputBase
+                      name="sellerApproval"
+                      label="Código"
+                      defaultValue={order.vendedorPendenteDiferenciado?.codigo}
+                      readOnly
+                    />
 
-                  <InputBase
-                    name="sellerAllName"
-                    label="Nome"
-                    defaultValue={order.vendedorPendenteDiferenciado?.nome}
-                    readOnly
-                  />
-                  <InputBase
-                    name="sellerAllName"
-                    label="Hierarquia"
-                    defaultValue={
-                      order.vendedorPendenteDiferenciado?.tipoVendedor
-                    }
-                    readOnly
-                  />
-                </DetailBox>
-              )}
+                    <InputBase
+                      name="sellerAllName"
+                      label="Nome"
+                      defaultValue={order.vendedorPendenteDiferenciado?.nome}
+                      readOnly
+                    />
+                    <InputBase
+                      name="sellerAllName"
+                      label="Hierarquia"
+                      defaultValue={
+                        order.vendedorPendenteDiferenciado?.tipoVendedor
+                      }
+                      readOnly
+                    />
+                  </DetailBox>
+                )}
 
               {order?.situacaoPedido?.codigo === 6 &&
                 order.vendedorPendenteDiferenciadoCodigo ===
